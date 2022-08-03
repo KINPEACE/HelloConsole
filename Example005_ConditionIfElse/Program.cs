17770daecada203cb78201836c74637a77fa0f1b
KINPEACE <nikita_1997152@mail.ru>
2022-08-04 02:58:18 +0300

    add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ариша")
+{
+    Console.WriteLine("Ура, это же АРИША!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
